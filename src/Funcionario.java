76a44463a6a46bf035f60a309e79c3a0ed26061a
Matheus Teixeira <matheus_4086@UIRAPURU.BR>
2024-04-23 09:47:53 -0300

    Initial commit

---
+++ src/Funcionario.java
@@ -0,0 +1,12 @@
+public class Funcionario extends PessoaFisica {
+
+    private String cartao;
+
+    public String setCartao(String cartao) {
+        this.cartao = cartao;
+    }
+
+    public String getCartao() {
+        return (cartao);
+    }
+}
